ef32cc1885ce3d94df3f0341b37b367711e541ba
Md-Shahzaib-Dev <muhammad-shahzaib@iinv.tech>
2023-08-23 05:26:03 -0700

    initial commit create simple crud app with redux.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTask, updateTask, deleteTask } from './redux/actions/taskActions';
 
-function App() {
+const App = () => {
+  const tasks = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
+
+  const [taskText, setTaskText] = useState('');
+  const [editingTask, setEditingTask] = useState(null);
+
+  const handleAddTask = () => {
+    if (taskText.trim() !== '') {
+      if (editingTask) {
+        dispatch(updateTask({ ...editingTask, text: taskText }));
+        setEditingTask(null);
+      } else {
+        dispatch(addTask({ id: Date.now(), text: taskText }));
+      }
+      setTaskText('');
+    }
+  };
+
+  const handleEditTask = (task) => {
+    setTaskText(task.text);
+    setEditingTask(task);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    dispatch(deleteTask(taskId));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Management App</h1>
+      <input
+        type="text"
+        value={taskText}
+        onChange={(e) => setTaskText(e.target.value)}
+        placeholder="Enter task..."
+      />
+      <button onClick={handleAddTask}>{editingTask ? 'Update Task' : 'Add Task'}</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => handleEditTask(task)}>Edit</button>
+            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
